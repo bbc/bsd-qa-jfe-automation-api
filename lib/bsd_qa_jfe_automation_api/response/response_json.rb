757b80898d682af47263d8a70936fbe40985fb52
kelvininukbbc <kelvin.inuk@bbc.co.uk>
2018-06-28 15:48:22 +0100

    response parsing support for {[]}

--- lib/bsd_qa_jfe_automation_api/response/response_json.rb
+++ lib/bsd_qa_jfe_automation_api/response/response_json.rb
@@ -1,6 +1,6 @@
 module BsdQaJfeAutomationApi
   module Response
-    # response class for parsing json responses
+    # response class for parsing json responses {}
     class ResponseJson
       attr_reader :code, :body, :headers, :results, :errors
       def initialize(response)
@@ -9,6 +9,16 @@ module BsdQaJfeAutomationApi
         @headers = response.headers
         @results = @body.results || []
         @errors = @body.errors || []
+      end
+    end
+
+    # response class for parsing json responses [{}]
+    class ResponseJson2
+      attr_reader :code, :body, :headers
+      def initialize(response)
+        @code = response.code
+        @body = JSON.parse(response.body).map { |hash| Hashie::Mash.new(hash) }
+        @headers = response.headers
       end
     end
   end
